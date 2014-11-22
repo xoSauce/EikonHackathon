9adcfcd93b5b3fd42ec6b211640e8e1a2f57460f
Xelnect <dido.jordanov7@gmail.com>
2014-11-22 23:39:19 +0200

    displaying html with no resources

--- app/run.py
+++ app/run.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
+
+
 @app.route('/')
-@app.route('/hello/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
+
 if __name__ == '__main__':
     app.run()
+
