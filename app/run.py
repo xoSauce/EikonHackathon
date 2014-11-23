c708e5caee7df298106cbbe6472527600cd9f458
Xelnect <dido.jordanov7@gmail.com>
2014-11-23 02:20:35 +0200

    working css but no js

--- app/run.py
+++ app/run.py
@@ -1,14 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
-
+  app.run(debug=True)
